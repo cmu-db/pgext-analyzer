1aba99afb6852c5c4cec40943259ee0be661a93f
Abigale Kim <abigale.kim@tiledb.com>
2024-05-29 16:37:22 -0700

    documentation for util scripts

--- util/json_list.py
+++ util/json_list.py
@@ -1,5 +1,6 @@
-# Takes a string and separates it into a list of strings,
-# separated by space
+# This script takes a string and separates it into a list of strings, separated
+# by space. I used this tool to get lists of pg_regress tests from the 
+# extensions' Makefiles.
 
 input1 = input()
 words_list = input1.split()
